1951229374dcef4299de8423b17c280e811132c5
bokhuuu <zurabokhua@gmail.com>
2024-04-19 17:23:59 +0400

    Refactor SocialMediaList component to fetch and display data automatically
    - Modify SocialMediaList component to use useEffect hook for fetching data on mount and setting up an interval for periodic data updates
    - Extract fetchData function to handle data fetching logic
    - Implement cleanup function to clear interval on component unmount

--- src/components/SocialMediaList.jsx
+++ src/components/SocialMediaList.jsx
@@ -4,11 +4,24 @@ import { fetchSocialMedia } from "../api/fetchSocialMedia";
 const SocialMediaList = () => {
   const [socialMediaList, setSocialMediaList] = useState([]);
 
+  // Function to fetch social media entries
+  const fetchData = async () => {
+    try {
+      const data = await fetchSocialMedia();
+      setSocialMediaList(data);
+    } catch (error) {
+      console.error("Error fetching social media:", error);
+    }
+  };
+
+  // Effect to fetch data when the component mounts and every 1 seconds
   useEffect(() => {
-    // Fetch social media entries when the component mounts
-    fetchSocialMedia()
-      .then((data) => setSocialMediaList(data))
-      .catch((error) => console.error("Error fetching social media:", error));
+    fetchData(); // Fetch data when the component mounts
+
+    const intervalId = setInterval(fetchData, 1000); // Fetch data every 1 second
+
+    // Cleanup function to clear interval when component unmounts
+    return () => clearInterval(intervalId);
   }, []);
 
   return (
